7e4b50d69414578ae7b9c80895c600dc93f8f43e
Matthew Hooker <mwhooker@gmail.com>
2012-03-12 14:14:05 -0700

    checkpoint.

--- resources.py
+++ resources.py
@@ -1,21 +1,11 @@
-class Resource(object):
+from werkzeug import urls
 
-    __slots__ = ['resource']
 
-    def __init__(self, resource, encoding='ascii'):
-        if not isinstance(resource, basestring):
-            raise TypeError("resource must be string or unicode.")
-        if isinstance(resource, str):
-            self.resource = resource.decode(encoding=encoding)
-        else:
-            self.resource = resource
-
-    def to_ascii(self):
-        return self.resource.encode('punycode')
-
-    def split(self):
+class IRI(object):
+    def __init__(self, iri, charset='utf-8'):
         pass
 
-    @staticmethod
-    def build(scheme, netloc, path, params,  query, fragment):
-        pass
+class URI(object):
+    def __init__(self, uri, charset='ascii'):
+        if isinstance(uri, IRI):
+            uri = uri.to_uri()
