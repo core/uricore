cfb7e1a2b9d1860a94f658bbda2a098c5cb5b771
Matt Chisholm <matt@theory.org>
2012-03-12 14:36:14 -0700

    tests based on http://paste.pocoo.org/show/564758/

---
+++ tests/test_.py
@@ -0,0 +1,68 @@
+from httpcore.uri import URI
+from httpcore.iri import IRI
+from httpcore.datastructures import MultiDict
+
+
+class TestResources(unittest.TestCase):
+
+    def test_iri(self):
+        iri = IRI(u'http://\N{SNOWMAN}/')
+        assert repr(iri) == "IRI(u'http://\u2603/')"
+        assert iri.netloc == u'http://\u2603/'
+        assert iri.hostname == '\u2603'
+        assert iri.port is None
+        assert iri.path == u'/'
+
+    def test_iri_add_port(self):
+        iri = IRI(u'http://\N{SNOWMAN}/')
+        new_iri = iri.replace(port=8000)
+        assert repr(new_iri) == "IRI(u'http://\u2603:8000/')"
+        assert iri.port is None
+
+    def test_iri_update_query(self):
+        iri = IRI(u'http://\N{SNOWMAN}/')
+        iriq = iri.update_query({'foo': u'42'})
+        assert repr(iri.query) == "MultiDict()"
+        assert repr(iriq) == "IRI(u'http://\u2603/?foo=42')"
+        assert repr(iriq.query) == "MultiDict([('foo', '42')])"
+        iriq2 = iriq.update_query(foo=None)
+        assert repr(iriq2) == "IRI(u'http://\u2603/')"
+        assert repr(iriq.query) == "MultiDict([('foo', '42')])"
+
+    def test_uri(self):
+        iri = IRI(u'http://\N{SNOWMAN}/')
+        uri = URI(iri)
+        assert repr(uri) == "URI('http://xn--n3h/')"
+
+    def test_hashability(self):
+        iri = IRI(u'http://\N{SNOWMAN}/')
+        uri = URI('http://xn--n3h/')
+        assert hash(iri) != hash(uri)
+
+    def test_configurable_multi_dict_class(self):
+        class CustomMultiDict(MultiDict):
+            pass
+        iri = IRI(u'http://\N{SNOWMAN}/', query_class=CustomMultiDict)
+        assert isinstance(iri.query, CustomMultiDict)
+
+    def test_join_iri(self):
+        iri_d = IRI(u'http://\N{SNOWMAN}/')
+        iri_p = IRI(u'/path/to/thing')
+        iri_j = iri_d.join(iri_p)
+        assert repr(iri_j) == "IRI(u'http://\u2603/path/to/thing')"
+
+    def test_join_uri(self):
+        uri_d = URI('http://\N{SNOWMAN}/')
+        uri_p = URI('/path/to/thing')
+        uri_j = uri_d.join(uri_p)
+        assert repr(uri_j) == "URI('http://\u2603/path/to/thing')"
+
+    def test_cant_join_strings(self):
+        iri_d = IRI(u'http://\N{SNOWMAN}/')
+        self.assertRaises(TypeError, iri_d.join, u'/path/to/thing')
+        uri_d = URI(u'http://\N{SNOWMAN}/')
+        self.assertRaises(TypeError, uri_d.join, '/path/to/thing')
+
+    def test_from_lenient(self):
+        lenient_iri = IRI.from_lenient(u'http://de.wikipedia.org/wiki/Elf (Begriffskl\xe4rung)')
+        assert repr(lenient_iri) = "URI('http://de.wikipedia.org/wiki/Elf%20%28Begriffskl%C3%A4rung%29')"
