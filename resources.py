2da37356f6dfff222f72df2b425522a63480db12
Matthew Hooker <mwhooker@gmail.com>
2012-03-12 14:30:49 -0700

    checkpoint

--- resources.py
+++ resources.py
@@ -2,17 +2,27 @@
 from __future__ import unicode_literals
 
 
-class IRI(object):
+class _RI(object):
+    def __init__(self, query_class=None):
+        self.decoder
 
-    def __init__(self, iri, charset='utf-8'):
-        pass
+
+class IRI(_RI):
+
+    def __init__(self, iri, charset='utf-8', query_class=None):
+        super(IRI, self).__init__(query_class)
+        if isinstance(iri, URI):
+            iri = iri.to_iri()
 
     def to_uri(self):
         pass
 
+class URI(_RI):
 
-class URI(object):
-
-    def __init__(self, uri, charset='ascii'):
+    def __init__(self, uri, query_class=None):
+        super(IRI, self).__init__(query_class)
         if isinstance(uri, IRI):
             uri = uri.to_uri()
+
+    def to_iri(self):
+        pass
