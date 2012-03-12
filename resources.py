bb4016c70d8ac9d6027fcfb78124f55db0190855
jeremy avnet <brainsik-github@theory.org>
2012-03-12 14:20:59 -0700

    don't import werkzeug and make a stub

--- resources.py
+++ resources.py
@@ -1,11 +1,18 @@
-from werkzeug import urls
+# encoding: utf-8
+from __future__ import unicode_literals
 
 
 class IRI(object):
+
     def __init__(self, iri, charset='utf-8'):
         pass
 
+    def to_uri(self):
+        pass
+
+
 class URI(object):
+
     def __init__(self, uri, charset='ascii'):
         if isinstance(uri, IRI):
             uri = uri.to_uri()
